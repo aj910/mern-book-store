3982375d27074c814353d861f9c51ee8b2fe23ba
aj910 <anmolj2009@gmail.com>
2023-10-04 12:22:36 +0530

    http requests added as middleware routes

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://root:root@mern-book-store.fmbuv8a.mongodb.net/books-collection?retryWrites=true&w=majority';
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,36 @@
-import express from "express";
-import { PORT } from "../backend/config.js";
+import express, { request } from "express";
+import { PORT, mongoDBURL } from "../backend/config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import bookRoute from "./routes/bookRoute.js"
 
 const app = express();
-app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`);
+
+app.use(express.json())
+
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send("Welcome to mern book store app!")
 })
+
+app.use('/books', bookRoute)
+
+
+const connectWithRetry = () => {
+    mongoose.connect(mongoDBURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(() => {
+        console.log("App connection with database")
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`);
+        });
+    }).catch((error) => {
+        console.log('MongoDB Connection Error: ', error);
+        setTimeout(connectWithRetry, 5000)
+    });
+};
+
+connectWithRetry();
+
+
