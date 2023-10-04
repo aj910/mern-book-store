b33f4793a952207ac45e5edda00f8d7ed4a658ef
aj910 <anmolj2009@gmail.com>
2023-10-04 18:15:39 +0530

    create book form reset

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -3,14 +3,15 @@ import BackButton from "../components/BackButton";
 import Spinner from "../components/Spinner";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { enqueueSnackbar, useSnackbar } from "notistack";
+import { useSnackbar } from "notistack";
 
 const CreateBook = () => {
     const [title, setTitle] = useState('');
     const [author, setAuthor] = useState('');
     const [publishYear, setPublishYear] = useState('');
-    const [isloading, setIsLoading] = useState('');
+    const [isloading, setIsLoading] = useState(false);
     const navigate = useNavigate();
+    const {enqueueSnackbar} = useSnackbar();
 
     const handleSaveBook = () => {
         const data = {
@@ -22,19 +23,22 @@ const CreateBook = () => {
         axios.post('http://localhost:5555/books', data).then(() => {
             setIsLoading(false);
             enqueueSnackbar('Book created successfully', { variant: 'success' });
-            navigate('/')
+            navigate('/');
+            document.getElementById('bookForm').reset();
         })
             .catch((error) => {
                 setIsLoading(false);
                 enqueueSnackbar('Error', { variant: 'error' });
-                console.log(error)
-            })
+                console.log(error);
+            });
     }
     return (
         <div className="p-4">
+           
             <BackButton />
             <h1 className="text-2xl text-center my-4">Create Book</h1>
             {isloading ? <Spinner /> : ''}
+            <form id="bookForm">
             <div className="flex flex-col border-4 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
                 <div className="my-4">
                     <label className="text-lg mr-4 text-gray-500">Title</label>
@@ -48,10 +52,10 @@ const CreateBook = () => {
                     <label className="text-lg mr-4 text-gray-500">Publish Year</label>
                     <input type="text" value={publishYear} className="border-2 border-gray-500 px-4 py-2 w-full" onChange={(e) => setPublishYear(e.target.value)} />
                 </div>
-              
                     <button onClick={handleSaveBook} className="p-3 w-[250px] rounded-lg mx-auto bg-sky-300 m-5">Save</button>
-              
             </div>
+            </form>
+            
         </div>
     )
 }
