41ced215db0cac0a1f616fff532d109376c6d64f
aj910 <anmolj2009@gmail.com>
2023-10-13 14:28:07 +0530

    build project

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/mern-book-store/",
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    minify: 'terser',
-  },
+  
 })
